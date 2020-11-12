f7c7275f58c77c1ddb84201f2c4a35efec6c0d90
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-11-12 13:44:25 -0800

    Done.

--- index.js
+++ index.js
@@ -51,9 +51,12 @@ const [r, o, y, g, b, i, v] = colors;
 
 // 6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg. 
 
+const [, , , , , indg, ] = colors;
+
 // Objects
 
 // 7. Use destructuring to assign all appropriate variables using the keys as the variable names
 
+const {name, color, song, job, partner} = muppet;
 
 // 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
